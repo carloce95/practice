06bb06f1199f55463e2f795e1ff8ed43bf7e3661
carloce95 <53699409+carloce95@users.noreply.github.com>
2021-02-15 14:20:05 +0100

    w

---
+++ Aplicacion.java
@@ -0,0 +1,9 @@
+/*   EJERCICIO:
+  Realizar una aplicación que elija aleatoriamente a los participantes
+  de una clase para "salir a la pizarra" (contestar cuestiones o dar respuestas
+  a ejercicios). El programa contiene los nombres de los asistentes y un 
+  menú en el que las opciones, de momento, son:
+     1. Elegir alumno.
+     0. Salir
+     
+*/
